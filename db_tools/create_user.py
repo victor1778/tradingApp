76689f167877992d7008e34c1a90a8f4b631e732
Victor Vega <68914862+victor1778@users.noreply.github.com>
2022-03-14 13:04:39 -0400

    Update create_user.py

--- db_tools/create_user.py
+++ db_tools/create_user.py
@@ -1,5 +1,4 @@
 import auth
 
-#mycursor.execute("CREATE DATABASE mydatabase")
 auth.mycursor.execute("CREATE OR REPLACE USER user@'%' IDENTIFIED BY 'password'")
 auth.execute("GRANT ALL PRIVILEGES ON *.* TO user@'%' IDENTIFIED BY 'password'")
