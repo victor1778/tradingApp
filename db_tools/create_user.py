d2b11e8ceac7b259fdc15be53780190ee9434475
Victor Vega <68914862+victor1778@users.noreply.github.com>
2022-03-14 14:40:59 -0400

    commit on dev

--- db_tools/create_user.py
+++ db_tools/create_user.py
@@ -1,4 +1,6 @@
 import auth
 
+#change user in user@'%' for username, change '%' to specify database access, and change password in 'password' to set user passwd.
 auth.mycursor.execute("CREATE OR REPLACE USER user@'%' IDENTIFIED BY 'password'")
-auth.execute("GRANT ALL PRIVILEGES ON *.* TO user@'%' IDENTIFIED BY 'password'")
+auth.mycursor.execute("GRANT ALL PRIVILEGES ON *.* TO user@'%' IDENTIFIED BY 'password'")
+
