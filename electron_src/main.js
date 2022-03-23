6bd4297d9f6b1dc2924f8bb831876d4676dc2f50
Rodolfo Samqui <rsamqui501@gmail.com>
2022-03-22 23:37:40 -0600

    Pagina de inicio modificada

--- electron_src/main.js
+++ electron_src/main.js
@@ -1,10 +1,10 @@
-const {app, BrowserWindow} = require('electron')
+const {app,BrowserWindow} = require('electron')
 
 function createWindow()
 {
     const win=new BrowserWindow({
-        width:900,
-        height:600
+        width:1920,
+        height:1080
     })
     win.loadFile('index.html')
 }
