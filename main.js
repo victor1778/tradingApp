b9dc83638632acd4af0777244050928e969ccb67
rsamqui <70598277+rsamqui@users.noreply.github.com>
2022-03-23 00:30:59 -0500

    Agregado inde.html, main.js y styles.css

---
+++ main.js
@@ -0,0 +1,12 @@
+const {app,BrowserWindow} = require('electron')
+
+function createWindow()
+{
+    const win=new BrowserWindow({
+        width:1920,
+        height:1080
+    })
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
